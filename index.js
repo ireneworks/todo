f936826dc6c918e3c29a746bfcc8b813417f4da1
ireneworks <hello@ireneworks.com>
2021-09-11 19:38:32 +0900

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const addNewTodo = document.getElementById("todo-submit")
 addNewTodo.addEventListener("click", noticeNewTodo);
 
 function noticeNewTodo(){
-    let newTodoContent = document.getElementById("new-todo-content");
+    let newTodoContent = document.getElementById("new-todo-content").value;
     alert(newTodoContent)
 }
