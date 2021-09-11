6e942aaae61cc7046d9b8f81fcceb3b6982aa643
ireneworks <hello@ireneworks.com>
2021-09-11 22:12:50 +0900

    update

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const submitNewTodo = document.getElementById("todo-submit")
 submitNewTodo.addEventListener("click", addNewTodo);
 
 function addNewTodo() {
-    let newTodoContent = document.getElementById("new-todo-content").value;
-    if(newTodoContent === ""){
-        alert("할 일을 입력하세요.");
-    }
-    else{
-        alert(`[${newTodoContent}]할 일이 등록되었습니다.`);
-    }
-    const todoList = document.getElementById("todo-list");
-    const addNewTodoList = document.createElement("li");
-    addNewTodoList.value = newTodoContent;
-    todoList.appendChild(addNewTodoList);
+    const newTodoContent = document.getElementById("new-todo-content").value;
 
+    function addNewTodoToList(){
+        //li 엘리먼트 생성
+        const addNewList = document.createElement("li");
+        //text 노드 추가
+        addNewList.appendChild(document.createTextNode(newTodoContent));
+        //ol에 li 추가
+        document.getElementById("todo-list").appendChild(addNewList);
+    }
+    addNewTodoToList();
+    // if (newTodoContent !== "") {
+    //     addNewTodoToList();
+    //     alert(`[${newTodoContent}]할 일이 등록되었습니다.`);
+    // }
+    // else {
+    //     alert("할 일을 입력하세요.");
+    // }
 }
