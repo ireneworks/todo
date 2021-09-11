5dcc479fff06f4fe4c03286e9cd6cc9be2b6095b
ireneworks <hello@ireneworks.com>
2021-09-11 20:37:12 +0900

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const addNewTodo = document.getElementById("todo-submit")
-addNewTodo.addEventListener("click", function(){
+const submitNewTodo = document.getElementById("todo-submit")
+submitNewTodo.addEventListener("click", addNewTodo);
+
+function addNewTodo() {
     let newTodoContent = document.getElementById("new-todo-content").value;
     if(newTodoContent === ""){
         alert("할 일을 입력하세요.");
     }
     else{
-       alert('['+newTodoContent+']'+"할 일이 등록되었습니다.");
+        alert(`[${newTodoContent}]할 일이 등록되었습니다.`);
     }
-});
+    const todoList = document.getElementById("todo-list");
+    const addNewTodoList = document.createElement("li");
+    addNewTodoList.value = newTodoContent;
+    todoList.appendChild(addNewTodoList);
+
+}
