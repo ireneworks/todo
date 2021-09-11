6f54678a7074e70d9bb92d704239322d21cd3470
ireneworks <hello@ireneworks.com>
2021-09-11 19:51:28 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const addNewTodo = document.getElementById("todo-submit")
-addNewTodo.addEventListener("click", noticeNewTodo);
-
-function noticeNewTodo(){
+addNewTodo.addEventListener("click", function(){
     alert("할 일을 입력해주세요.");
-}
+});
