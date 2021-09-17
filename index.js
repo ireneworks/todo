881b900805d1421763a41e9634ef4c09d8889493
ireneworks <hello@ireneworks.com>
2021-09-17 12:06:38 +0900

    update

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 const submitNewTodo = document.getElementById("new-todo");
 submitNewTodo.addEventListener("submit", addNewTodo);
 
-let newTodoContent = document.getElementById("new-todo-content");
-
-function addNewTodoToList(){
-    //엘리먼트 생성
-    const li = document.createElement("li");
-    li.className = "todo";
-
-    const checkbox = document.createElement("input");
-    checkbox.type = 'checkbox';
-    checkbox.className = "todo-checkbox";
-
-    const todoText = document.createElement("input");
-    todoText.type = "text";
-    todoText.value = "ad";
-
-    const date = document.createElement("p");
-    const deleteTodo = document.createElement("button");
-    // deleteTodo.type = "submit";
-    deleteTodo.className = "delete-todo";
-    //노드 추가
-    li.append(checkbox, deleteTodo);
-    date.append(`등록일: ${new Date()}`);
-    checkbox.after(date, todoText);
-    deleteTodo.append("삭제");
-
-    //ol에 li 추가
-    document.getElementById("todo-list").prepend(li);
-}
-
 function addNewTodo(e) {
     e.preventDefault();
 
+    let newTodoContent = document.getElementById("new-todo-content").value;
+
     if (newTodoContent.value !== "") {
         addNewTodoToList();
-        alert(`[${newTodoContent.value}]할 일이 등록되었습니다.`);
+        alert(`[${newTodoContent}]할 일이 등록되었습니다.`);
     }
     else {
         alert("할 일을 입력하세요.");
     }
-    return(newTodoContent.value);
+    function addNewTodoToList(){
+        //엘리먼트 생성
+        const li = document.createElement("li");
+        li.className = "todo";
+
+        const checkbox = document.createElement("input");
+        checkbox.type = 'checkbox';
+        checkbox.className = "todo-checkbox";
+
+        const todoText = document.createElement("input");
+        todoText.type = "text";
+        todoText.value = newTodoContent;
+
+        const date = document.createElement("p");
+        const deleteTodo = document.createElement("button");
+        deleteTodo.type = "submit";
+        deleteTodo.className = "delete-todo";
+        //노드 추가
+        li.append(checkbox, deleteTodo);
+        date.append(`등록일: ${new Date()}`);
+        checkbox.after(date, todoText);
+        deleteTodo.append("삭제");
+
+        //ol에 li 추가
+        document.getElementById("todo-list").prepend(li);
+    }
 }
 
-const list = document.getElementById("todo-list");
 const deleteButton = document.getElementsByClassName("delete-todo");
-deleteButton.addEventListener("click", deleteTodo);
-function deleteTodo (){
-    list.removeChild(document.getElementsByClassName("todo"));
+deleteButton.addEventListener("submit", deleteTodo);
+function deleteTodo(e){
+    e.preventDefault();
+    const li = document.querySelector('li');
+    li.remove();
 }
