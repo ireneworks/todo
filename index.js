a05e7633dc81ea4c9afe57005fb62e144c5d50b7
ireneworks <hello@ireneworks.com>
2021-09-17 10:27:22 +0900

    update

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const submitNewTodo = document.getElementById("todo-submit")
-submitNewTodo.addEventListener("click", addNewTodo);
+const submitNewTodo = document.getElementById("new-todo");
+submitNewTodo.addEventListener("submit", addNewTodo);
 
-function addNewTodo() {
-    const newTodoContent = document.getElementById("new-todo-content").value;
+let newTodoContent = document.getElementById("new-todo-content");
 
-    function addNewTodoToList(){
-        //li 엘리먼트 생성
-        const li = document.createElement("li");
-        //text 노드 추가
-        li.append(document.createTextNode(newTodoContent));
-        //ol에 li 추가
-        document.getElementById("todo-list").append(li);
-    }
+function addNewTodoToList(){
+    //엘리먼트 생성
+    const li = document.createElement("li");
+    li.className = "todo";
 
-    if (newTodoContent !== "") {
+    const checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.className = "todo-checkbox";
+
+    const todoText = document.createElement("input");
+    todoText.type = "text";
+    todoText.value = "ad";
+
+    const date = document.createElement("p");
+    const deleteTodo = document.createElement("button");
+    // deleteTodo.type = "submit";
+    deleteTodo.className = "delete-todo";
+    //노드 추가
+    li.append(checkbox, deleteTodo);
+    date.append(`등록일: ${new Date()}`);
+    checkbox.after(date, todoText);
+    deleteTodo.append("삭제");
+
+    //ol에 li 추가
+    document.getElementById("todo-list").prepend(li);
+}
+
+function addNewTodo(e) {
+    e.preventDefault();
+
+    if (newTodoContent.value !== "") {
         addNewTodoToList();
-        alert(`[${newTodoContent}]할 일이 등록되었습니다.`);
+        alert(`[${newTodoContent.value}]할 일이 등록되었습니다.`);
     }
     else {
         alert("할 일을 입력하세요.");
     }
+    return(newTodoContent.value);
 }
 
-const textChanges = document.getElementById("new-todo-content").value;
-textChanges.addEventListener("change", function(){
-    const p = document.createElement("p");
-    p.innerHTML = "Jeff";
-    document.getElementById("hello").appendChild(p);
-});
+const list = document.getElementById("todo-list");
+const deleteButton = document.getElementsByClassName("delete-todo");
+deleteButton.addEventListener("click", deleteTodo);
+function deleteTodo (){
+    list.removeChild(document.getElementsByClassName("todo"));
+}
